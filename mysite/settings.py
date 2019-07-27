32facbf8f498a8af85ab84ad0a25668a8d46e316
Sumon <sumonkantidey23@gmail.com>
2019-07-27 18:08:20 +0600

    added django-heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,4 @@ LOGIN_REDIRECT_URL = '/' #after login user will redirect to home page
 
 
 DEBUG = True
+django_heroku.settings(locals())
